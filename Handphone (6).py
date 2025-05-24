49dbb88095859c83de4e9b8e9f6de4ca674cfb92
nahibatullah <nahibatullah.own@gmail.com>
2025-05-24 11:24:37 +0700

    Add files via upload

---
+++ Handphone (6).py
@@ -0,0 +1,51 @@
+class Handphone:
+    def __init__(self, merek, model, tahun, kapasitas_baterai):
+        self.merek = merek
+        self.model = model
+        self.tahun = tahun
+        self.kapasitas_baterai = kapasitas_baterai  # dalam mAh
+        self.status_daya = 100  # persentase
+        self.dinyalakan = False
+
+    # Method untuk menyalakan handphone
+    def nyalakan(self):
+        if self.status_daya > 0:
+            self.dinyalakan = True
+            print(f"{self.merek} {self.model} telah dinyalakan.")
+        else:
+            print("Baterai kosong. Silakan isi daya terlebih dahulu.")
+
+    # Method untuk mematikan handphone
+    def matikan(self):
+        self.dinyalakan = False
+        print(f"{self.merek} {self.model} telah dimatikan.")
+
+    # Method untuk mengisi daya
+    def isi_daya(self, jumlah):
+        self.status_daya = min(100, self.status_daya + jumlah)
+        print(f"Handphone diisi daya. Status baterai sekarang: {self.status_daya}%")
+
+    # Method untuk menampilkan info handphone
+    def info(self):
+        status = "Menyala" if self.dinyalakan else "Mati"
+        print(f"Handphone: {self.merek} {self.model}")
+        print(f"Tahun: {self.tahun}")
+        print(f"Baterai: {self.status_daya}% dari {self.kapasitas_baterai}mAh")
+        print(f"Status: {status}")
+
+#Objek Pertama
+hp1 = Handphone("Samsung", "Galaxy S23", 2023, 5000)
+hp1.info()
+hp1.nyalakan()
+hp1.isi_daya(-20)
+hp1.info()
+
+print("\n")
+
+#Objek Kedua
+hp2 = Handphone("Apple", "iPhone 14", 2022, 4352)
+hp2.info()
+hp2.matikan()
+hp2.isi_daya(-90)
+hp2.nyalakan()
+hp2.info()
